a443becce5f15096567d6df9441f870a1c7684db
sivamani242004 <varadhasivamani2@gmail.com>
2025-07-28 14:09:29 +0530

    update changes in daily payments

--- src/main/java/com/mrtech/adminportal/repository/StudentRepository.java
+++ src/main/java/com/mrtech/adminportal/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.mrtech.adminportal.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,9 @@ import com.mrtech.adminportal.entity.Student;
 public interface StudentRepository extends JpaRepository<Student, Integer> 
 { 
 	List<Student> findByNameContainingIgnoreCaseOrMobileContaining(String name, String mobile);
+
+	Optional<Student> findById(Long id);
+
+	Student findByNameContainingIgnoreCase(String name);
+
 }
