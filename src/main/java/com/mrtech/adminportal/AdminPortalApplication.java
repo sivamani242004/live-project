bdabf3d6d83d64ca987668e6f844b2cc1e2fe452
Mahesh0227 <maheshadhapureddy1216@gmail.com>
2025-12-09 03:35:23 +0530

    final report works

--- src/main/java/com/mrtech/adminportal/AdminPortalApplication.java
+++ src/main/java/com/mrtech/adminportal/AdminPortalApplication.java
@@ -3,14 +3,10 @@ package com.mrtech.adminportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
 @EnableScheduling
 public class AdminPortalApplication {
-
 	public static void main(String[] args) {
-
 		 SpringApplication.run(AdminPortalApplication.class, args);
 	}
-  
 }
